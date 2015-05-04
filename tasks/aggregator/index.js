a078a41edc2cf498f4383aa2e35edc8018c8e739
tdeekens <tobias.deekens@epost-dev.de>
2015-05-04 16:48:23 +0200

    Add support for unknown licenses.

--- tasks/aggregator/index.js
+++ tasks/aggregator/index.js
@@ -1,10 +1,20 @@
 'use strict';
 
 var
+  _processUnknown,
   _ = require('lodash');
 
 function Aggregator() {
 }
+
+_processUnknown = function(licenses) {
+  return _.map(licenses, function(license) {
+    return (
+      license === null || license === undefined ?
+        'Unknown' : license
+    );
+  });
+};
 
 Aggregator.prototype.get = function(packages) {
   var _output = {
@@ -16,7 +26,7 @@ Aggregator.prototype.get = function(packages) {
     var _licenses = _.isArray(packageInfo.licenses) ?
       packageInfo.licenses : [packageInfo.licenses];
 
-    _.each(_licenses, function(license) {
+    _.each(_processUnknown(_licenses), function(license) {
       _output.licenses[license] = _output.licenses[license] || 0;
       _output.packages[license] = _.uniq(_output.packages[license], true) || [];
 
